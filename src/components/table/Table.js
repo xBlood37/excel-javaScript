f7e97fef3e8e0e496c7f49eb32a02516f3b5ab7c
xBlood37 <123d.123b@mail.ru>
2022-09-01 17:39:13 +0300

    resize finish

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,25 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from './table.tamplate';
 
+import {resizeHandler} from './table.resize';
+import {shouldResize} from './table.function';
+
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable(15);
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
   }
 }
