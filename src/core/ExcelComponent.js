98dea43dd647837b8f778684ca1f89c813c0d6da
xBlood37 <123d.123b@mail.ru>
2022-09-06 22:15:35 +0300

    table-logic-finish

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,18 +4,40 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emitter = options.emitter;
+    this.unsubscribers = [];
+
+    this.prepare();
   }
+
+  // setings components
+  prepare() {}
 
   // return sample components
   toHTML() {
     return '';
   }
 
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args);
+  }
+
+  // subscribe event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+
+  // initialize sample components
+  // add dom listeners
   init() {
     this.initDomListeners();
   }
 
+  // destroy sample components
+  // destroy dom listeners
   destroy() {
     this.removeDomListeners();
+    this.unsubscribers.forEach((unsub) => unsub());
   }
 }
