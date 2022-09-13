7899a5f793a3bc68752a1eacc0a61cdb33e2503a
xBlood37 <123d.123b@mail.ru>
2022-09-13 14:18:01 +0300

    routing finish

---
+++ src/pages/ExcelPage.js
@@ -0,0 +1,47 @@
+import {Page} from '@core/page';
+
+import {createStore} from '../core/createStore';
+import {rootReducer} from '../redux/rootReducer';
+import {storage, debounce} from '@core/utils';
+import {normalizeInitialState} from '../redux/initialState';
+
+import {Excel} from '@/components/excel/Excel';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Formula} from '@/components/formula/Formula';
+import {Table} from '@/components/table/Table';
+
+function storageName(param) {
+  return 'excel:' + param;
+}
+
+export class ExcelPage extends Page {
+  getRoot() {
+    const params = this.params ? this.params : Date.now().toString();
+
+    const state = storage(storageName(params));
+    const initialState = normalizeInitialState(state);
+    const store = createStore(rootReducer, initialState);
+
+    const stateLitener = debounce((state) => {
+      storage(storageName(params), state);
+    }, 300);
+
+    store.subscribe(stateLitener);
+
+    this.excel = new Excel({
+      components: [Header, Toolbar, Formula, Table],
+      store,
+    });
+
+    return this.excel.getRoot();
+  }
+
+  afterRender() {
+    this.excel.init();
+  }
+
+  destroy() {
+    this.excel.destroy();
+  }
+}
